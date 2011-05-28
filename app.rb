f4c063bdef41aaf72b5b16d90ba4e04ad0abfa68
Arian Ryan <arian@beenverified.com>
2011-05-28 17:05:37 -0400

    added basic auth; production server config

--- app.rb
+++ app.rb
@@ -8,6 +8,14 @@ class App < Sinatra::Application
   enable :sessions
   use Rack::Flash
   
+  ENV['RACK_ENV'] = File.open('local_config/env.config').read
+  
+  USERS = {
+    :arian   => ['arian', 'R0ckstar!'],
+    :susan   => ['susan', 'soozyQ101'],
+    :kathryn => ['kathryn', 'thatswhatshesaid']
+  }
+  
   configure do
     $deployment = File.open('local_config/env.config').read.to_sym
     set :environment, $deployment
@@ -22,7 +30,50 @@ class App < Sinatra::Application
   before do
   end
   
+  before %r{/.+} do
+    set_user! unless logged_in?
+    protected!
+  end
+  
   get '/' do
     erb :index
   end
+  
+  get '/home' do
+    "Welcome, #{user}"
+  end
+  
+  
+  helpers do
+    def protected!
+      unless logged_in?
+        response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+        throw(:halt, [401, "Not authorized\n"])
+      end
+    end
+    
+    def set_user!
+      auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      if auth.provided? && auth.basic? && auth.credentials
+        session[:user] = USERS.invert[auth.credentials]
+      end
+    end
+    
+    def unset_user!
+      session.delete(:user)
+    end
+    
+    def user
+      session[:user] ? session[:user].to_s.capitalize : nil
+    end
+    
+    def logged_in?
+      session[:user] ? true : false
+    end
+    
+    def partial(template, locals = {})
+      erb(template, :layout => false, :locals => locals, :cache => false)
+    end
+  end
+  
 end
